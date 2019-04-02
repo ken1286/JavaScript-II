d9c7ff034a17561be52fd37b1858ab3aab92e647
ken1286 <kindle1286@gmail.com>
2019-04-02 13:02:03 -0700

    Callbacks assignment complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,29 +27,78 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(arrLength) {
+  console.log(arrLength);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+
+last(items, function(lastItem) {
+  console.log(lastItem);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(2, 3, function(sum) {
+  console.log(sum);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(2, 5, function(result) {
+  console.log(result);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item, list);
 }
+
+  const cbContains = (item, list) => {
+    for(i = 0; i < list.length; i++) {
+      if(item === list[i]) {
+        return result = true;
+      }   
+    }
+    return result = false;
+  }
+
+  const cbContainsForEach = function(item, list) {
+    result = false;
+    list.forEach(function(element) {
+      if(element === item) {
+        result = true;
+      }
+    });
+    return result;
+  }
+
+  console.log(contains("Pencil", items, cbContains));
+  console.log(contains("Bill Bob", items, cbContains));
+  console.log(contains('yo-yo', items, cbContains));
+
+  console.log(contains("Pencil", items, cbContainsForEach));
+  console.log(contains("Bill Bob", items, cbContainsForEach));
+  console.log(contains('yo-yo', items, cbContainsForEach));
+
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+// function removeDuplicates(array, cb) {
+//   // removeDuplicates removes all duplicate values from the given array.
+//   // Pass the duplicate free array to the callback function.
+//   // Do not mutate the original array.
+// }
