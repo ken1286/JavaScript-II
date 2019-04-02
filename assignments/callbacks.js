0d52da7f04cb99312f81e16e519ed042fe92c717
ken1286 <kindle1286@gmail.com>
2019-04-02 13:55:35 -0700

    Array methods challenge done

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -54,7 +54,7 @@ sumNums(2, 3, function(sum) {
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-  return cb(x * y);
+  return cb(x, y);
 }
 
 multiplyNums(2, 5, function(result) {
